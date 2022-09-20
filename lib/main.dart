b4423b1264234f631e4206de7c5140eb5c88ee57
deaveto <davt94@gmail.com>
2022-09-19 22:07:13 -0500

    comentario1

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hoteles SD',      
-      theme: ThemeData.dark(),
+      title: 'Hoteles SD',
+      theme: ThemeData.dark(), //Sindi
       home: homePage(),
     );
   }
