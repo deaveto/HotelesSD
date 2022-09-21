2ce49ef32b9d2b05239ec7547d7b3f9bba3dcf36
deaveto <davt94@gmail.com>
2022-09-21 13:05:53 -0500

    commit2

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hoteles SD',      
+      title: 'Hoteles SD',
       theme: ThemeData.light(),
       home: homePage(),
     );
