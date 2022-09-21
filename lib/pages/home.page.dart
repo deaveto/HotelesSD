3b86d3c484fc8857885b93dc468f38d6232455dd
deaveto <davt94@gmail.com>
2022-09-21 13:06:10 -0500

    commit3

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -11,7 +11,18 @@ class _homePageState extends State<homePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Hoteles SD")),
+      //appBar: AppBar(title: Text("Hoteles SD")),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            //fit: BoxFit.fitWidth,
+            fit: BoxFit.fitHeight,
+            image: NetworkImage("assets/images/hotel.jpg"),
+          ),
+        ),
+      ),
     );
   }
 }
