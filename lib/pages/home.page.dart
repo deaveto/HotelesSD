dd25fae88b0fc8e0b302b4d7d5809d08d958836e
deaveto <davt94@gmail.com>
2022-09-21 16:08:40 -0500

    comit4

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class homePage extends StatefulWidget {
@@ -17,9 +19,55 @@ class _homePageState extends State<homePage> {
         height: double.infinity,
         decoration: BoxDecoration(
           image: DecorationImage(
-            //fit: BoxFit.fitWidth,
-            fit: BoxFit.fitHeight,
+            fit: BoxFit
+                .fitHeight, //Configure la altura de la fuente para que coincida con el ancho del cuadro de destino. Puede hacer que la fuente desborde el cuadro de destino horizontalmente.
+            colorFilter: ColorFilter.mode(
+                Colors.black.withOpacity(0.4),
+                BlendMode
+                    .dstATop), //Establecer transparencia/opacidad de la imagen
             image: NetworkImage("assets/images/hotel.jpg"),
+          ),
+        ),
+        child: Container(
+          height: 10,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage("assets/images/logo.png"),
+            ),
+          ),
+          child: Column(
+            children: [
+              Expanded(
+                flex: 9,
+                child: Container(
+                  child: Row(
+                    children: [
+                      Text(""),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: Colors.blue,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(children: [
+                        Text(
+                          "Consultar",
+                          style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ]),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
